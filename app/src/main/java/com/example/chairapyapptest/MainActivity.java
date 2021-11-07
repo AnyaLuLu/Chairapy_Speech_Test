8f41d36c590efae44d7da9e72cb689ad1174bd06
Anya <a56lu@uwaterloo.ca>
2021-11-07 18:02:11 -0500

    Different Try on getting the mic to work

--- app/src/main/java/com/example/chairapyapptest/MainActivity.java
+++ app/src/main/java/com/example/chairapyapptest/MainActivity.java
@@ -3,15 +3,76 @@ package com.example.chairapyapptest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+
+    TextView mTextTv;
+    ImageButton mVoiceBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextTv = findViewById(R.id.textTV);
+        mVoiceBtn = findViewById(R.id.voiceBtn);
+
+
+        mVoiceBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                speak();
+
+            }
+        });
+
+    }
+
+    private void speak(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speaketh");
+
+
+        try{
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        }
+        catch(Exception e){
+            Toast.makeText(this, "Not Recognized"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case REQUEST_CODE_SPEECH_INPUT:{
+                if(resultCode == RESULT_OK && null != data){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    mTextTv.setText(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+
+    /*
 
     private TextView txvResult;
 
@@ -28,10 +89,10 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
+        if(intent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(intent, 10);
         } else {
-            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Your Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -48,4 +109,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+     */
 }
